5f08aa1b84c8f7e19196dc3016b62337b28e0f24
EeDSan <sazon.eugene@gmail.com>
2020-10-08 20:50:47 +0300

    feature-load-updates: remove tests for load_updates method

--- scrapper/tests/test_updater.py
+++ scrapper/tests/test_updater.py
@@ -1,8 +1,4 @@
-import pickle
 import unittest
-
-from scrapper.updater import Updater
-
 
 class UpdaterTest(unittest.TestCase):
 
@@ -14,13 +10,3 @@ class UpdaterTest(unittest.TestCase):
 
     def test_unregister_observer(self):
         assert True
-
-    def test_load_updates(self):
-        with open('test_load_updates_excepted_value.txt', 'rb') as file:
-            excepted_value = pickle.load(file)
-        self.assertListEqual(excepted_value, Updater.load_updates(1097150))
-
-    def test_load_updates_raise_exception(self):
-        with self.assertRaises(Exception) as EXCEPTION:
-            Updater.load_updates(111)
-        self.assertTrue('rss feed doesn`t exist' in str(EXCEPTION.exception))
