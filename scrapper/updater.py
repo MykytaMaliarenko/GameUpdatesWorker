e587b938f16969feb2d87001dc8a1c49b2effbb6
EeDSan <sazon.eugene@gmail.com>
2020-10-08 20:50:25 +0300

    feature-load-updates: refactor load_updates method, changed to private

--- scrapper/updater.py
+++ scrapper/updater.py
@@ -20,15 +20,15 @@ class Updater:
         pass
 
     @staticmethod
-    def load_updates(game_id: int) -> List[UpdateInfo]:
-        list_of_updates = []
+    def __load_updates(game_id: int) -> List[UpdateInfo]:
         feed = feedparser.parse("https://store.steampowered.com/feeds/newshub/app/" + str(game_id))
-        for elem in feed.feed.links:
-            if 'https://store.steampowered.com/newshub/app/' + str(game_id) + '/' not in elem.values():
-                raise Exception('rss feed doesn`t exist')
-        for entry in feed.entries:
-            update_title = entry.title
-            update_published_at = entry.published
-            update_description = entry.summary
-            list_of_updates.append(UpdateInfo(update_title, update_description, update_published_at, game_id))
-        return list_of_updates
+        if feed.status != 200 or len(feed.entries) == 0:
+            raise Exception('rss feed doesn`t exist')
+
+        return [
+            UpdateInfo(title=entry.title,
+                       description=entry.summary,
+                       publication_date=entry.published,
+                       game_id=game_id)
+            for entry in feed.entries
+        ]
