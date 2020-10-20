caf86a11369c148f661653fe4a977c6d15ba65f3
Mykyta Maliarenko <mykytamaliarenko@gmail.com>
2020-10-20 18:09:31 +0300

    add local env

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
-if __name__ == "__main__":
+import os
+import json
+
+LOCAL_ENV = "env_local.json"
+
+
+def init_local_env(path: str):
+    if os.path.exists(path):
+        with open(path) as json_file:
+            data = json.load(json_file)
+            for key, value in data.items():
+                os.environ[key] = value
+
+
+def main():
     pass
+
+
+if __name__ == "__main__":
+    init_local_env(LOCAL_ENV)
+    main()
