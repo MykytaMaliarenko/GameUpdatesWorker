67c76e4ccbbbe710f7d8e92190dd6043059e6c4b
EeDSan <sazon.eugene@gmail.com>
2020-10-29 18:37:23 +0200

    feat-observer-pattern: add observer implementation and main logic to scrapper, resolve conflicts

--- scrapper/scrapper.py
+++ scrapper/scrapper.py
@@ -1,6 +1,10 @@
 import datetime
 import time
-from typing import List
+import aiohttp
+import asyncio
+
+from bs4 import BeautifulSoup
+from typing import List, Dict
 import feedparser
 
 from db.models import Game
@@ -42,15 +46,45 @@ class Scrapper:
             self.__if_game_has_new_update()
             time.sleep(60)
 
+    @staticmethod
+    async def __get_metadata(session, url: str, result: List[Dict[str, str]]):
+        async with session.get(url) as response:
+            raw = await response.text()
+            soup = BeautifulSoup(raw, features="html.parser")
+            result.append({
+                'description': soup.find("meta", property="og:description")["content"],
+                'image_url': soup.find("meta", property="og:image")["content"]
+            })
+
+    @staticmethod
+    def __get_all_metadata(urls: List[str]):
+        res = list()
+        routines = list()
+
+        loop = asyncio.get_event_loop()
+        session = aiohttp.ClientSession(loop=loop)
+        for url in urls:
+            routines.append(loop.create_task(Scrapper.__get_metadata(session, url, res)))
+
+        loop.run_until_complete(asyncio.wait(routines))
+        loop.run_until_complete(asyncio.wait([session.close()]))
+        loop.close()
+        return res
+
     def __load_updates(self) -> List[UpdateInfo]:
-        feed = feedparser.parse("https://store.steampowered.com/feeds/newshub/app/" + str(self.game.id))
+        feed = feedparser.parse("https://store.steampowered.com/feeds/newshub/app/" + str(self.game))
         if feed.status != 200 or len(feed.entries) == 0:
             raise Exception('rss feed doesn`t exist')
+
+        entries_metedata = Scrapper.__get_all_metadata([entry.link for entry in feed.entries])
+
         return [
             UpdateInfo(title=entry.title,
                        description=entry.summary,
                        publication_date=datetime.datetime(*(entry.published_parsed[0:6])),
-                       game_id=self.game.id,
-                       origin_url='')
-            for entry in feed.entries
+                       game_id=self.game,
+                       origin_url=entry.link,
+                       short_description=entries_metedata[index]['description'],
+                       image_url=entries_metedata[index]['image_url'])
+            for index, entry in enumerate(feed.entries)
         ]
