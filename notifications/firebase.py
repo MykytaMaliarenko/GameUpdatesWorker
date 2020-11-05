f702c6807b41b5b3e14db0f9fb30cad52a852abe
Mykyta Maliarenko <mykytamaliarenko@gmail.com>
2020-11-05 15:45:22 +0200

    ref FirebasePopupNotifications: add inheritance  from AbstractObserver

--- notifications/firebase.py
+++ notifications/firebase.py
@@ -1,9 +1,13 @@
 from db.models import User
+from scrapper.observer import AbstractObserver
 from scrapper.updateinfo import UpdateInfo
 
 
-class FirebasePopupNotifications:
+class FirebasePopupNotifications(AbstractObserver):
     __firebase_server_key: str
+
+    def update(self, update_info: UpdateInfo):
+        pass
 
     @staticmethod
     def notify_user(user: User, update_info: UpdateInfo):
