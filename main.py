114bebd16e10909cf697f4b2e3688b0a58bb0528
Mykyta Maliarenko <mykytamaliarenko@gmail.com>
2020-11-06 18:30:25 +0200

    feat: add loguru support

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
 import json
+import sys
+from loguru import logger
 
 LOCAL_ENV = "env_local.json"
 
@@ -12,10 +14,33 @@ def init_local_env(path: str):
                 os.environ[key] = value
 
 
+def init_logger():
+    if 'logger_log_to' in os.environ:
+        logger.remove()
+
+        if 'console' in os.environ['logger_log_to']:
+            logger.add(sys.stdout,
+                       colorize=bool(os.environ['logger_console_colorized']),
+                       format=os.environ['logger_console_format'],
+                       level=os.environ['logger_console_level'])
+
+        if 'file' in os.environ['logger_log_to']:
+            logger.add(
+                os.environ["logger_file_name"],
+                rotation=os.environ["logger_file_rotation"])
+
+        if 'file_json' in os.environ['logger_log_to']:
+            logger.add(
+                os.environ["logger_file_json_name"],
+                rotation=os.environ["logger_file_json_rotation"],
+                serialize=True)
+
+
 def main():
     pass
 
 
 if __name__ == "__main__":
     init_local_env(LOCAL_ENV)
+    init_logger()
     main()
